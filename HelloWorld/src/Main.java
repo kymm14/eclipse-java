26319edd154a5165f643ea125670c08d21374915
Yone <yonetzymm@gmail.com>
2023-02-24 14:04:36 +0100

    commit

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("2*3");
+		System.out.println(2*3);
 	}
 
 }
